9f4900e46b4a14b2cd37a4ae6d17cc12257e642d
Vladimir Neradovskikh <56476783+vladimir-skikh@users.noreply.github.com>
2019-11-10 18:11:05 +0300

    Добавлены скрипты

---
+++ js/main.js
@@ -0,0 +1,49 @@
+/* $(function() {
+  $('.header-video__button').on('click', function() {
+    $('.header-video-poster').css({"display":"none"});
+    let video = $('.header-video');
+    video.css({'display':'block'});
+    video.get(0).play();
+  });
+  $('.my-slider').slick({
+    vertical: true,
+    verticalSwiping: true,
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 1,
+  });
+}); */
+
+let items = document.querySelectorAll('.services-item');
+var service = function (items_arr) {
+  for (let i = 0; i < items_arr.length; i++) {
+    item = items_arr[i];
+    item.addEventListener('click', function () {
+      if (!(this.classList.contains('active'))) {
+        for (let i = 0; i < items_arr.length; i++) {
+          items_arr[i].classList.remove('active');
+        };
+      };
+      this.classList.add('active');
+    });
+  };
+};
+service(items);
+
+let participants = document.querySelectorAll('.participant-image');
+let participants_info = document.querySelectorAll('.participant__info-block');
+const participant = (participants_arr, participants_info_arr) => {
+  for (let i = 0; i < participants_arr.length; i++) {
+    let images_block = document.querySelector('.images__block');
+    participants_arr[i].onclick = function () {
+      let info = '-info';
+      let id = this.id;
+      for (let j = 0; j < participants_info_arr.length; j++) {
+        participants_info_arr[j].classList.remove('active');
+      }
+      let current_info = document.querySelector(`#${id}${info}`);
+      current_info.classList.add('active');
+    };
+  };
+};
+participant(participants, participants_info);
